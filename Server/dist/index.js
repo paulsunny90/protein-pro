5aa1504fc4ee96448fbb8eacb9ca694d431280e5
paulsunny90 <paulsunny87267@gmail.com>
2026-02-03 21:40:27 +0530

    server created

--- Server/dist/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 50000;
-const start = async () => {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-};
