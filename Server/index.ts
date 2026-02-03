03cbb062eff2ffaa7959b0d07ff211a3849ba290
paulsunny90 <paulsunny87267@gmail.com>
2026-02-03 21:35:44 +0530

    ser er created

---
+++ Server/index.ts
@@ -0,0 +1,11 @@
+import app  from "./app.ts";
+
+const PORT = process.env.PORT||50000
+
+const start = async ()=>{
+
+    app.listen(PORT,()=>{
+        console.log(`Server running on http://localhost:${PORT}`)
+    })
+}
+start()
