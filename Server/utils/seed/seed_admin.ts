069b8127429f9912b729efe212d326891045c97a
paulsunny90 <paulsunny87267@gmail.com>
2026-02-10 17:40:32 +0530

    Updated admin seed

--- Server/utils/seed/seed_admin.ts
+++ Server/utils/seed/seed_admin.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Userlog from "./models/user.model.ts";
+import Userlog from "../../models/user.model.ts";
 import bcrypt from "bcryptjs";
 
 dotenv.config();
