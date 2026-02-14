28e6909c6993597d013ea5bb706a33162ff55cba
paulsunny90 <paulsunny87267@gmail.com>
2026-02-14 11:10:14 +0530

    backend ready for deploy

--- Server/index.ts
+++ Server/index.ts
@@ -1,12 +1,12 @@
-import app  from "./app.ts";
+import app from "./app.ts";
 import connectDB from "./config/db.config.ts"
 
 const PORT = process.env.PORT || 5000
 
 
-const start = async ()=>{
-    await  connectDB();
-    app.listen(PORT,()=>{
+const start = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`)
     })
 }
