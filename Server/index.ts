be24fefa514c8f155cac785b254f62a26dfb0ef8
paulsunny90 <paulsunny87267@gmail.com>
2026-02-15 14:38:54 +0530

    server updated index.ts  diploying

--- Server/index.ts
+++ Server/index.ts
@@ -1,13 +1,20 @@
 import app from "./app.ts";
-import connectDB from "./config/db.config.ts"
+import connectDB from "./config/db.config.ts";
 
-const PORT = process.env.PORT 
-
+const PORT = process.env.PORT || 5000; 
 
 const start = async () => {
+  try {
     await connectDB();
+
     app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`)
-    })
-}
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Server failed to start:", error);
+    process.exit(1);
+  }
+};
+
 start();
