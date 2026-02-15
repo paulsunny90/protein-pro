c32bf619663216d94f42cdfb128a23d862b44312
paulsunny90 <paulsunny87267@gmail.com>
2026-02-15 16:17:25 +0530

    serve client uri  updated   diploying

--- Server/app.ts
+++ Server/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: [process.env.CLIENT_URL || "http://localhost:5173", "http://localhost:5173", "http://localhost:5174"], // Vite's ports
+  origin: [process.env.CLIENT_URL || "https://protein-pro-client-79c3.vercel.app"], // Vite's ports
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
