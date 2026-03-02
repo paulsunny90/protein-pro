8725db5f27e5fd0a7540b88d2a11cbc7a6e68aac
paulsunny90 <paulsunny87267@gmail.com>
2026-03-02 15:23:17 +0530

    data updated in iamages all

--- Server/config/upload.config.ts
+++ Server/config/upload.config.ts
@@ -1,12 +1,17 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Ensure upload directory exists
 const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
+
 
 // Multer Local disk storage configuration
 const storage = multer.diskStorage({
